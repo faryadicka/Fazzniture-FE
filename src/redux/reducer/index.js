d1487ac98e5f7101aecde3cb30a103bb227bb5a3
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-06-25 00:05:11 +0700

    Add register reducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import userReducer from "./user";
 import favoriteProductReducer from "./favorite";
+import registerReducer from "./register";
 
 const reducer = combineReducers({
   user: userReducer,
-  favorite: favoriteProductReducer
+  favorite: favoriteProductReducer,
+  register: registerReducer
 })
 
 export default reducer
