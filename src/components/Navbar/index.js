422061c43b431d8a98a30549d1f618cb72a4f592
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-06-24 21:02:03 +0700

    Update git ignore

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,18 +6,18 @@ export class Navbar extends Component {
         return (
             <Fragment>
                 <div className='navbar'>
-                    <div>
+                    <div className='col-12 col-md-4'>
                         <div className='navbar-1'>
                             FAZZNITURE
                         </div>
                     </div>
-                    <div className='navbar-2'>
+                    <div className='navbar-2 col-12 col-md-4'>
                         <div className='navbar-2-content'>Home</div>
                         <div className='navbar-2-content'>Pages</div>
                         <div className='navbar-2-content'>Shop</div>
                         <div className='navbar-2-content'>Blog</div>
                     </div>
-                    <div className='navbar-3'>
+                    <div className='navbar-3 col-12 col-md-4'>
                         <div><img src={require("../../assets/vector/Vector-Search.png")} alt="Search" className="navbar-3-logo"/></div>
                         <div><img src={require("../../assets/vector/Vector-Love.png")} alt="Love" className="navbar-3-logo"/></div>
                         <div><img src={require("../../assets/vector/Vector-Cart.png")} alt="Cart" className="navbar-3-logo"/></div>
