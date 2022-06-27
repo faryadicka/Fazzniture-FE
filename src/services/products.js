85ae445b8635f7bdce5521c14b4998cbfa3e4bc4
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-06-28 00:46:55 +0700

    Create upload product

--- src/services/products.js
+++ src/services/products.js
@@ -5,3 +5,14 @@ export const getProductsAxios = (name = "", category = "", size = "", brand = ""
   const URL = `${REACT_APP_HOST}/product?category=${category}&size=${size}&color=${color}&brand=${brand}&max_range=${max}&min_range=${min}&sort=${sort}&order=${order}&page=${page}&limit=12`
   return axios.get(URL)
 }
+
+export const createNewProductAxios = (body, token) => {
+  const URL = `${REACT_APP_HOST}/product`
+  console.log("fuck", body)
+  return axios.post(URL, body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "multipart/form-data",
+    }
+  })
+}
