9d34b0d57e8a89e81f4845b8da038c06012a1a19
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-06-28 20:10:47 +0700

    Update Reset Password

--- src/pages/Reset/index.jsx
+++ src/pages/Reset/index.jsx
@@ -1,66 +1,71 @@
 import React from 'react';
 import './resetPassword.css';
-import axios from 'axios'
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 import Navbar from '../../components/Navbar'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import BlackButton from '../../components/Black-Button';
+import Modal from '../../components/Modal';
+import {authResetAxios} from "../../services/auth"
+
 const ResetPassword = ()=>{
-    
+    const [showModal, setShowModal] = useState(false)
     const [newPassword, setNewPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
     const {token} = useParams()
-    useEffect(()=>{
-        console.log(token);
-    })
-    const resetPassword = async ()=>{
-        try {
-            const config = {
-                headers : {
-                    Authorization : `Bearer ${token}`
-                }
-            }
-            const body = {
-                newPassword,
-                confirmPassword
-            }
-            const result = axios.put(`http://localhost:8000/auth/reset-password`, body, config)
-            console.log(result);
-            console.log(token);
-        } catch (error) {
-            console.log(error);
+    const navigate = useNavigate()
+    const resetPassword = ()=>{
+        const body = {
+            newPassword,
+            confirmPassword
         }
+        authResetAxios(body, token)
+        .then(() => {
+            setShowModal(true)
+        })
+        .catch( (error) => {
+            navigate("/404");
+        })
     }
 
     return (
+        <div>
+        <Navbar/>
+        <Header title="My Account"/>
         <div className="forgot-main">
-            <Navbar/>
-            <Header title="My Account"/>
-           <div className='forgotMain'>
+           <div>
               <div className="forgot-title forgot-color-black forgot-arimo-30">Reset your password</div>
               <div className="forgot-color-black forgot-arimo-12">Please input your new Password</div>
-              <label className="forgot-input-container">
-                 <input type="text" placeholder="New Password" className="forgot-input forgot-arimo-12" onChange={e=>{
-                    setNewPassword(e.target.value)
-                 }} />
-              </label>
-              <label className="forgot-input-container">
-                 <input type="text" placeholder="Confirm Password" className="forgot-input forgot-arimo-12" onChange={e=>{
-                    setConfirmPassword(e.target.value)
-                 }}/>
-              </label> 
-              <div className="resetButton" onClick={resetPassword}>Reset</div>
+              <div className='reset-container'>
+                <label className="forgot-input-container">
+                    <input type="text" placeholder="Set your new password" className="forgot-input forgot-arimo-12" 
+                    onChange={(e)=>{
+                        setNewPassword(e.target.value)
+                    }}/>
+                </label>
+                <label className="forgot-input-container">
+                    <input type="text" placeholder="Confirm your new password" className="forgot-input forgot-arimo-12" 
+                    onChange={(e)=>{
+                        setConfirmPassword(e.target.value)
+                    }}/>
+                </label>
+              </div>
+              <div className="sendLink" onClick={resetPassword}><BlackButton text="Reset"/></div>
               <div
                  className="forgot-color-black forgot-arimo-12 forgot-back"
-              >
+                 onClick={() => {
+                    navigate('/auth');
+                 }}>
                  Back to Login
               </div>
            </div>
-           <Footer/>
+           {showModal ? <Modal title="Succes!" message="Please login to your account" button="Login" navigateTo="/auth" close="false" /> : <></>}
         </div>
-     );
+        <Footer/>
+        </div>
+    );
 }
 
 export default ResetPassword;
